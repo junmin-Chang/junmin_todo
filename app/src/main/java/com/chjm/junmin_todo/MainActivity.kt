fd18363bed339def8cc828bd54a4a33a62a9e6ac
junminChang <chjm219@kookmin.ac.kr>
2020-08-04 22:07:01 +0900

    layout_setting & adapter

---
+++ app/src/main/java/com/chjm/junmin_todo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.chjm.junmin_todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+data class Todo(val text: String, var isDone: Boolean)
+
+
+class TodoAdapter(private val myDataset: List<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+
+    class TodoViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): TodoAdapter.TodoViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_todo, parent, false)
+
+
+        return TodoViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val textView = holder.view.findViewById<TextView>(R.id.todo_text)
+        textView.text = myDataset[position].text
+    }
+
+
+    override fun getItemCount() = myDataset.size
+}
